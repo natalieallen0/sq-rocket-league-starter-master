6073fb1033e38dfa5086d63aadc84f67481ec5ad
natalieallen0 <137357875+natalieallen0@users.noreply.github.com>
2023-07-28 11:40:52 -0500

    Rename

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This file is for strategy
-#Rocket League Bot (Nataie Allen)
+# Rocket League Bot (Nataie Allen)
 
 from util.objects import *
 from util.routines import *
@@ -11,29 +11,46 @@ class Bot(GoslingAgent):
     def run(self):
         if self.intent is not None:
             return
+
         ball_distance = abs(self.ball.location.y - self.foe_goal.location.y)
         self_distance = abs(self.me.location.y - self.foe_goal.location.y)
         is_in_front_of_ball = ball_distance > self_distance
+
         if self.kickoff_flag:
             # set_intent tells the bot what it's trying to do
             self.set_intent(kickoff())
             return
-        
+
+        if self.is_in_front_of_ball():
+            self.set_inent(goto(self.friend_goal.location))
+        self.set_inent(goto(self.foe_goal.location))
+
+        if self.me.boost > 99:
+            self.set_intent(short_shot(self.foe_goal.location))
+
+        target_boost = self.get_closest_large_boost()
+        if target_boost is not None:
+            self.set_intent(goto(target_boost.location))
+            return
+
         # if we are in front of the ball, retreat
         if is_in_front_of_ball:
             self.set_intent(goto(self.friend_goal.location))
             return
         self.set_intent(short_shot(self.foe_goal.location))
 
-        #playing around with lists
+        # playing around with lists
         targets = {
-            'at_opponent goal': (self.foe_goal.left_post, self.foe_goal.right_post),
-            'away_from_our_net': (self.friend_goal.right_post, self.friend_goal.left_post) 
+            "at_opponent goal": (self.foe_goal.left_post, self.foe_goal.right_post),
+            "away_from_our_net": (
+                self.friend_goal.right_post,
+                self.friend_goal.left_post,
+            ),
         }
         hits = find_hits(self, targets)
-        if len(hits['at_opponent goal']) > 0
-            self.set_intent(hits['at opponent goal'][0])
+        if len(hits["at_opponent goal"]) > 0:
+            self.set_intent(hits["at opponent goal"][0])
             return
-        if len(hits['away_from_our_net']) > 0
-            self.set_intent(hits['away_from_our_net'][0])
+        if len(hits["away_from_our_net"]) > 0:
+            self.set_intent(hits["away_from_our_net"][0])
             return
--- util/objects.py
+++ util/objects.py
@@ -6,6 +6,7 @@ from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 # This file holds all of the objects used in gosling utils
 # Includes custom vector and matrix objects
 
+
 class GoslingAgent(BaseAgent):
     # This is the main object of Gosling Utils. It holds/updates information about the game and runs routines
     # All utils rely on information being structured and accessed the same way as configured in this class
@@ -47,9 +48,16 @@ class GoslingAgent(BaseAgent):
     def refresh_player_lists(self, packet):
         # makes new freind/foe lists
         # Useful to keep separate from get_ready because humans can join/leave a match
-        self.friends = [car_object(i, packet) for i in range(packet.num_cars) if
-                        packet.game_cars[i].team == self.team and i != self.index]
-        self.foes = [car_object(i, packet) for i in range(packet.num_cars) if packet.game_cars[i].team != self.team]
+        self.friends = [
+            car_object(i, packet)
+            for i in range(packet.num_cars)
+            if packet.game_cars[i].team == self.team and i != self.index
+        ]
+        self.foes = [
+            car_object(i, packet)
+            for i in range(packet.num_cars)
+            if packet.game_cars[i].team != self.team
+        ]
 
     def set_intent(self, routine):
         self.intent = routine
@@ -70,7 +78,9 @@ class GoslingAgent(BaseAgent):
 
     def line(self, start, end, color=None):
         color = color if color != None else [255, 255, 255]
-        self.renderer.draw_line_3d(start.copy(), end.copy(), self.renderer.create_color(255, *color))
+        self.renderer.draw_line_3d(
+            start.copy(), end.copy(), self.renderer.create_color(255, *color)
+        )
 
     def debug_intent(self):
         # Draws the stack on the screen
@@ -84,19 +94,29 @@ class GoslingAgent(BaseAgent):
 
     def preprocess(self, packet):
         # Calling the update functions for all of the objects
-        if packet.num_cars != len(self.friends) + len(self.foes) + 1: self.refresh_player_lists(packet)
-        for car in self.friends: car.update(packet)
-        for car in self.foes: car.update(packet)
-        for pad in self.boosts: pad.update(packet)
+        if packet.num_cars != len(self.friends) + len(self.foes) + 1:
+            self.refresh_player_lists(packet)
+        for car in self.friends:
+            car.update(packet)
+        for car in self.foes:
+            car.update(packet)
+        for pad in self.boosts:
+            pad.update(packet)
         self.ball.update(packet)
         self.me.update(packet)
         self.game.update(packet)
         self.time = packet.game_info.seconds_elapsed
         # When a new kickoff begins we empty the stack
-        if self.kickoff_flag == False and packet.game_info.is_round_active and packet.game_info.is_kickoff_pause:
+        if (
+            self.kickoff_flag == False
+            and packet.game_info.is_round_active
+            and packet.game_info.is_kickoff_pause
+        ):
             self.clear_intent()
         # Tells us when to go for kickoff
-        self.kickoff_flag = packet.game_info.is_round_active and packet.game_info.is_kickoff_pause
+        self.kickoff_flag = (
+            packet.game_info.is_round_active and packet.game_info.is_kickoff_pause
+        )
 
     def get_output(self, packet):
         # Reset controller
@@ -116,6 +136,26 @@ class GoslingAgent(BaseAgent):
         self.renderer.end_rendering()
         # send our updated controller back to rlbot
         return self.controller
+
+    def get_closest_large_boost(self):
+        available_boosts = [
+            boost for boost in self.boosts if boost.large and boost.active
+        ]
+        closest_boost = None
+        closest_distance = 10000
+        for boost in available_boosts:
+            distance = (self.me.location - boost.location).magnitude()
+            if closest_boost is None or distance < closest_distance:
+                closest_boost = boost
+                closest_distance = distance
+        return closest_boost
+
+    def is_in_front_of_ball(self):
+        ball_distance = abs(self.ball.location.y - self.foe_goal.location.y)
+        self_distance = abs(self.me.location.y - self.foe_goal.location.y)
+        if self_distance < ball_distance + 1000:
+            return True
+        return False
 
     def run(self):
         # override this with your strategy code
@@ -146,14 +186,28 @@ class car_object:
 
     def update(self, packet):
         car = packet.game_cars[self.index]
-        self.location.data = [car.physics.location.x,
-                              car.physics.location.y, car.physics.location.z]
-        self.velocity.data = [car.physics.velocity.x,
-                              car.physics.velocity.y, car.physics.velocity.z]
+        self.location.data = [
+            car.physics.location.x,
+            car.physics.location.y,
+            car.physics.location.z,
+        ]
+        self.velocity.data = [
+            car.physics.velocity.x,
+            car.physics.velocity.y,
+            car.physics.velocity.z,
+        ]
         self.orientation = Matrix3(
-            car.physics.rotation.pitch, car.physics.rotation.yaw, car.physics.rotation.roll)
+            car.physics.rotation.pitch,
+            car.physics.rotation.yaw,
+            car.physics.rotation.roll,
+        )
         self.angular_velocity = self.orientation.dot(
-            [car.physics.angular_velocity.x, car.physics.angular_velocity.y, car.physics.angular_velocity.z]).data
+            [
+                car.physics.angular_velocity.x,
+                car.physics.angular_velocity.y,
+                car.physics.angular_velocity.z,
+            ]
+        ).data
         self.demolished = car.is_demolished
         self.airborne = not car.has_wheel_contact
         self.supersonic = car.is_super_sonic
@@ -186,10 +240,16 @@ class ball_object:
 
     def update(self, packet):
         ball = packet.game_ball
-        self.location.data = [ball.physics.location.x,
-                              ball.physics.location.y, ball.physics.location.z]
-        self.velocity.data = [ball.physics.velocity.x,
-                              ball.physics.velocity.y, ball.physics.velocity.z]
+        self.location.data = [
+            ball.physics.location.x,
+            ball.physics.location.y,
+            ball.physics.location.z,
+        ]
+        self.velocity.data = [
+            ball.physics.velocity.x,
+            ball.physics.velocity.y,
+            ball.physics.velocity.z,
+        ]
         self.latest_touched_time = ball.latest_touch.time_seconds
         self.latest_touched_team = ball.latest_touch.team
 
@@ -255,14 +315,17 @@ class Matrix3:
         self.data = (
             Vector3(cp * cy, cp * sy, sp),
             Vector3(cy * sp * sr - cr * sy, sy * sp * sr + cr * cy, -cp * sr),
-            Vector3(-cr * cy * sp - sr * sy, -cr * sy * sp + sr * cy, cp * cr))
+            Vector3(-cr * cy * sp - sr * sy, -cr * sy * sp + sr * cy, cp * cr),
+        )
         self.forward, self.left, self.up = self.data
 
     def __getitem__(self, key):
         return self.data[key]
 
     def dot(self, vector):
-        return Vector3(self.forward.dot(vector), self.left.dot(vector), self.up.dot(vector))
+        return Vector3(
+            self.forward.dot(vector), self.left.dot(vector), self.up.dot(vector)
+        )
 
 
 class Vector3:
@@ -377,13 +440,17 @@ class Vector3:
 
     def magnitude(self):
         # Magnitude() returns the length of the vector
-        return math.sqrt((self[0] * self[0]) + (self[1] * self[1]) + (self[2] * self[2]))
+        return math.sqrt(
+            (self[0] * self[0]) + (self[1] * self[1]) + (self[2] * self[2])
+        )
 
     def normalize(self):
         # Normalize() returns a Vector3 that shares the same direction but has a length of 1.0
         magnitude = self.magnitude()
         if magnitude != 0:
-            return Vector3(self[0] / magnitude, self[1] / magnitude, self[2] / magnitude)
+            return Vector3(
+                self[0] / magnitude, self[1] / magnitude, self[2] / magnitude
+            )
         return Vector3(0, 0, 0)
 
     # Linear algebra functions
@@ -392,8 +459,11 @@ class Vector3:
 
     def cross(self, value):
         # A .cross((0, 0, 1)) will rotate the vector counterclockwise by 90 degrees
-        return Vector3((self[1] * value[2]) - (self[2] * value[1]), (self[2] * value[0]) - (self[0] * value[2]),
-                       (self[0] * value[1]) - (self[1] * value[0]))
+        return Vector3(
+            (self[1] * value[2]) - (self[2] * value[1]),
+            (self[2] * value[0]) - (self[0] * value[2]),
+            (self[0] * value[1]) - (self[1] * value[0]),
+        )
 
     def flatten(self):
         # Sets Z (Vector3[2]) to 0
@@ -409,13 +479,18 @@ class Vector3:
 
     def angle(self, value):
         # Returns the angle between this Vector3 and another Vector3
-        return math.acos(round(self.flatten().normalize().dot(value.flatten().normalize()), 4))
+        return math.acos(
+            round(self.flatten().normalize().dot(value.flatten().normalize()), 4)
+        )
 
     def rotate(self, angle):
         # Rotates this Vector3 by the given angle in radians
         # Note that this is only 2D, in the x and y axis
-        return Vector3((math.cos(angle) * self[0]) - (math.sin(angle) * self[1]),
-                       (math.sin(angle) * self[0]) + (math.cos(angle) * self[1]), self[2])
+        return Vector3(
+            (math.cos(angle) * self[0]) - (math.sin(angle) * self[1]),
+            (math.sin(angle) * self[0]) + (math.cos(angle) * self[1]),
+            self[2],
+        )
 
     def clamp(self, start, end):
         # Similar to integer clamping, Vector3's clamp() forces the Vector3's direction between a start and end Vector3
@@ -424,7 +499,11 @@ class Vector3:
         s = self.normalize()
         right = s.dot(end.cross((0, 0, -1))) < 0
         left = s.dot(start.cross((0, 0, -1))) > 0
-        if (right and left) if end.dot(start.cross((0, 0, -1))) > 0 else (right or left):
+        if (
+            (right and left)
+            if end.dot(start.cross((0, 0, -1))) > 0
+            else (right or left)
+        ):
             return self
         if start.dot(s) < end.dot(s):
             return end
